b380e9f52d6a78c236b0215105c9ddab64579224
RainYang1765 <muhua1765@gmail.com>
2026-03-04 13:44:47 +0800

    Add Windows specific recommend option
    add option to set windows volume name after mount operation.
    
    Signed-off-by: RainYang1765 <muhua1765@gmail.com>

--- src/views/MountView.vue
+++ src/views/MountView.vue
@@ -641,6 +641,12 @@ const commonFlags = [
       { flag: '--progress', value: '', descriptionKey: 'progress' },
     ],
   },
+  {
+    category: 'Windows Specific',
+    flags: [
+      { flag: '--volname', value: 'OpenlistDAV', descriptionKey: 'volname' },
+    ],
+  },
 ]
 
 const filterList = computed(() => [
